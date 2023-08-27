933a5163e524c8e5b3e4c1c44a2366d01105cc57
Indusz <116461854+Indusz@users.noreply.github.com>
2023-08-27 07:48:12 -0400

    Delete bgchange.js

--- bgchange.js
+++
@@ -1,7 +0,0 @@
-<script>
-    function delayAndChangeBackground(imageUrl) {
-        setTimeout(function() {
-            document.getElementById('body').style.backgroundImage = `url(${imageUrl})`;
-        }, 4500);
-    }
-</script>
