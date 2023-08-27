a141e80fe7179b6e71cfbbd277a59670108ee6d6
Indusz <116461854+Indusz@users.noreply.github.com>
2023-08-27 07:53:03 -0400

    Update bgchange.js

--- bgchange.js
+++ bgchange.js
@@ -1,7 +1,16 @@
 <script>
-    function delayAndChangeBackground(imageUrl) {
-        setTimeout(function() {
-            document.getElementById('body').style.backgroundImage = `url(${imageUrl})`;
-        }, 4500);
-    }
+    // Attach event listeners to all relevant links
+    const links = document.querySelectorAll('.hover-text');
+    
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default link behavior
+            const imageUrl = 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzI5NjdxMnRsNjN1bTI0Mm44em42cWV4c2VscGF0YjU1Y2Y4bTJkbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5xtDarHj0ORdxfWtV6g/giphy.gif';
+
+            setTimeout(function() {
+                document.getElementById('body').style.backgroundImage = `url(${imageUrl})`;
+                window.location.href = link.getAttribute('href'); // Manually open the link after the delay
+            }, 4500); // 4.5 seconds in milliseconds
+        });
+    });
 </script>
