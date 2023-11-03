33cb844449e7dd26fcdd435596f30da493ad8c3d
Indusz <116461854+Indusz@users.noreply.github.com>
2023-11-03 08:06:59 -0400

    Update movielol.js

--- movielol.js
+++ movielol.js
@@ -4,8 +4,10 @@ function toggleMute() {
     if (video.muted) {
         video.muted = false;
         audio.muted = true;
+        notification('Video unmuted, audio muted');
     } else {
         video.muted = true;
         audio.muted = false;
+        notification('Video muted, audio unmuted');
     }
 }
